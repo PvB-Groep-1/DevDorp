9f7c3409952a2850cdd81c670b96f6ccb9042163
Joey Kossen <joeykossen@hotmail.com>
2020-05-06 12:48:40 +0200

    <Feature: Main Menu> Add Quit game window Add Quit game functionality

---
+++ Assets/Scripts/Features/UI/MainMenuController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour
+{
+    [SerializeField] private GameObject _CloseGameConfirmationWindow = null;
+
+    public void ToggleWindowVisibility(GameObject window)
+    {
+        window.SetActive(!window.activeSelf);
+    }
+
+    public void CloseApplication()
+    {
+        Application.Quit();
+    }
+}
