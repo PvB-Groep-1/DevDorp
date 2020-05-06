adfa1a4a44ce201c96298d4d44b784e0a4fbd400
Joey Kossen <joeykossen@hotmail.com>
2020-05-06 12:58:48 +0200

    <feature: Quit Game> Add Comments for pull request

--- Assets/Scripts/Features/UI/MainMenuController.cs
+++ Assets/Scripts/Features/UI/MainMenuController.cs
@@ -2,15 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// This class controls the main menu.
+/// </summary>
 public class MainMenuController : MonoBehaviour
 {
-    [SerializeField] private GameObject _CloseGameConfirmationWindow = null;
-
+    /// <summary>
+    /// This function toggles the visibility for the given gameobject.
+    /// </summary>
+    /// <param name="window">Takes in the gameobject of which the visibilty has to be toggled.</param>
     public void ToggleWindowVisibility(GameObject window)
     {
         window.SetActive(!window.activeSelf);
     }
 
+    /// <summary>
+    /// This function controls everything for closing the game.
+    /// </summary>
     public void CloseApplication()
     {
         Application.Quit();
