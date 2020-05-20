709850d7219634f0053578a5ea5579e2c8b2b2da
Koen Sanderse <koensanderse2000@gmail.com>
2020-05-20 12:24:59 +0200

    <Feature: Rotate Building> Add BuildingRotation Command Add RotateLogic for blocks Add RotLogic parameter for blocks

---
+++ Assets/Scripts/Features/Gameplay/Block Programming/Logic/RotateLogic.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Represents rotation logic.
+/// </summary>
+public sealed class RotateLogic : ExecutionLogic
+{
+    [SerializeField]
+    private XYLogic _XYLogic;
+
+    [SerializeField]
+    private RotLogic _RotLogic;
+
+    public override void Execute()
+    {
+        if (_XYLogic.inputFieldX.text == "" || _XYLogic.inputFieldY.text == "" || _RotLogic.inputFieldRot.text == "")
+            return;
+
+        RotateObjectCommand command = new RotateObjectCommand(-15 + _XYLogic.x * 9, 5 + _XYLogic.y * 9, _RotLogic.rot);
+        command.Execute();
+    }
+    
+    public void SetValues()
+    {
+        _XYLogic.SetValues();
+        _RotLogic.SetValues();
+    }
+}
