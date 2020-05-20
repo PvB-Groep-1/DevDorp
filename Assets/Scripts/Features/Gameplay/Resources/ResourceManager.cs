5025fccc7df1311bd8567741b67a6af89a91f2f8
Joey Kossen <joeykossen@hotmail.com>
2020-05-20 13:51:49 +0200

    <Feature: Happiness> Add Backend happiness

---
+++ Assets/Scripts/Features/Gameplay/Resources/ResourceManager.cs
@@ -0,0 +1,20 @@
+﻿public static class ResourceManager
+{
+    public static float TotalHappiness = 30;
+
+    public static void IncreaseHappiness(float happiness)
+    {
+        if ((TotalHappiness + happiness) < 100)
+            TotalHappiness += happiness;
+        else
+            TotalHappiness = 100;
+    }
+
+    public static void DecreaseHappiness(float happiness)
+    {
+        if ((TotalHappiness - happiness) > 0)
+            TotalHappiness -= happiness;
+        else
+            TotalHappiness = 0;
+    }
+}
---
+++ Assets/Scripts/Features/UI/IngameUIManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class IngameUIManager : MonoBehaviour
+{
+    [SerializeField] private Image _HappinessBar;
+
+    private void FixedUpdate()
+    {
+        UpdateHappinessUI(ResourceManager.TotalHappiness);
+    }
+
+    private void UpdateHappinessUI(float happiness)
+    {
+        float fillAmount = _HappinessBar.fillAmount;
+        _HappinessBar.fillAmount = Mathf.Lerp(fillAmount, (happiness / 100), Time.deltaTime * 1.4f);
+    }
+}
