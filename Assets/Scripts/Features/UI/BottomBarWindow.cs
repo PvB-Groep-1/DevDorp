c5240e8ec60a4e31edae1c7f1671930dfecb2eea
Koen Sanderse <koensanderse2000@gmail.com>
2020-05-21 13:06:43 +0200

    <Feature: Grid Button> Add Code functionality in BottomBarWindow

--- Assets/Scripts/Features/UI/BottomBarWindow.cs
+++ Assets/Scripts/Features/UI/BottomBarWindow.cs
@@ -1,23 +1,48 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// Represents all functionality for the bottom UI bar.
 /// </summary>
 public class BottomBarWindow : MonoBehaviour
 {
-	/// <summary>
-	/// Opens the code window.
-	/// </summary>
-	public void OpenCodeWindow()
-	{
-		WindowApi.OpenWindow(WindowTypes.CodeMenu);
-	}
+    /// <summary>
+    /// All types of buttons in the BottomBarWindow.
+    /// </summary>
+    public enum ButtonTypes
+    {
+        BlockButton,
+        HomeButton,
+        GridButton
+    }
 
-	/// <summary>
-	/// Opens the main menu scene.
-	/// </summary>
-	public void OpenMainMenu()
-	{
-		LevelManager.LoadMainMenu();
-	}
+    [SerializeField]
+    private Button _gridButton;
+
+    [SerializeField]
+    private GameObject _gridObject;
+
+    /// <summary>
+    /// Opens the code window.
+    /// </summary>
+    public void OpenCodeWindow()
+    {
+        WindowApi.OpenWindow(WindowTypes.CodeMenu);
+    }
+
+    /// <summary>
+    /// Opens the main menu scene.
+    /// </summary>
+    public void OpenMainMenu()
+    {
+        LevelManager.LoadMainMenu();
+    }
+
+    /// <summary>
+    /// Toggles the grid GameObject
+    /// </summary>
+    public void ToggleGrid()
+    {
+        _gridObject.SetActive(!_gridObject.activeSelf);
+    }
 }
