0471bbdb3a50419167cd0c1e547c79edc782a88c
Joey Kossen <joeykossen@hotmail.com>
2020-06-02 12:58:38 +0200

    <Feature: Resource Notification> Add notifications that show when there are too little bread for the amount of villagers

--- Assets/Scripts/Features/Gameplay/Resources/ResourceDistributor.cs
+++ Assets/Scripts/Features/Gameplay/Resources/ResourceDistributor.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
@@ -18,6 +19,19 @@ public class ResourceDistributor : MonoBehaviour
     // The amount the happiness has to be decreased with for each annoyed villager.
     [SerializeField]
     private float _HappinessDecreasePerMadVillager = 2;
+
+    // The notification object.
+    [SerializeField]
+    private GameObject _ResourceNotification = null;
+
+    // Array used to save all the villagers who are currently walking around.
+    private GameObject[] villagers = null;
+
+    // Array used to save all the notifications currently in the game scene.
+    private List<GameObject> notifications = new List<GameObject>();
+
+    // A check to see if any notifications have been spawned.
+    private bool _instantiatedNotifications = false;
 
     private void Awake()
     {
@@ -54,11 +68,29 @@ public class ResourceDistributor : MonoBehaviour
             happinessIncrease = (_HappinessIncreasePerHappyVillager * ResourceManager.GetResourceAmount(ResourceType.villagers)) * (Time.deltaTime / 120);
 
             ResourceManager.IncreaseResource(ResourceType.happiness, happinessIncrease);
+
+            if(_instantiatedNotifications)
+            {
+                for (int i = 0; i < notifications.Count; i++)
+                {
+                    Destroy(notifications[i]);
+                }
+
+                villagers = null;
+                notifications.Clear();
+
+                _instantiatedNotifications = false;
+            }
         }
         else
         {
             int extraVillagers = (int)ResourceManager.GetResourceAmount(ResourceType.villagers) - (int)ResourceManager.GetResourceAmount(ResourceType.bread);
             float happinessDecrease = (extraVillagers * _HappinessDecreasePerMadVillager) * (Time.deltaTime / 60);
+
+            if (!_instantiatedNotifications)
+                SpawnNotifications(extraVillagers);
+
+            NotificationFollowVillager();
 
             happinessIncrease = (_HappinessIncreasePerHappyVillager * ResourceManager.GetResourceAmount(ResourceType.villagers)) * (Time.deltaTime / 120);
 
@@ -72,6 +104,36 @@ public class ResourceDistributor : MonoBehaviour
                 happinessDecrease -= happinessIncrease;
                 ResourceManager.DecreaseHappiness(happinessDecrease);
             }
+        }
+    }
+
+    /// <summary>
+    /// This function spawns the notifications.
+    /// </summary>
+    /// <param name="extraVillagers">Takes in the amount of notifications take have to be spawned.</param>
+    private void SpawnNotifications(int extraVillagers)
+    {
+        _instantiatedNotifications = true;
+
+        villagers = GameObject.FindGameObjectsWithTag("Villager");
+
+        for (int i = 0; i < extraVillagers; i++)
+        {
+            Vector3 Screenpos = Camera.main.WorldToScreenPoint(villagers[i].transform.position);
+            GameObject notification = Instantiate(_ResourceNotification, Screenpos, Quaternion.identity);
+
+            notification.transform.parent = GameObject.FindGameObjectWithTag("NotificationContainer").transform;
+
+            notifications.Add(notification);
+        }
+    }
+    
+    // This function makes the notifications follow the villager.
+    private void NotificationFollowVillager()
+    {
+        for (int i = 0; i < notifications.Count; i++)
+        {
+            notifications[i].transform.position = Camera.main.WorldToScreenPoint(villagers[i].transform.position) + new Vector3(0, 135, 0);
         }
     }
 
